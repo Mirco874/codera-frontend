0aeabbe8a5c13a605e326ad5f3849f5cc85715fc
Mirco Estefano Pilco Peralta <mircocuof@gmail.com>
2023-01-15 22:56:03 -0400

    added not same password message

--- src/auth/components/RegisterForm/RegisterForm.jsx
+++ src/auth/components/RegisterForm/RegisterForm.jsx
@@ -1,26 +1,17 @@
-import { useForm } from "../../../hooks";
 import { Button } from "../../../ui/components"
 import './RegisterForm.css'
 
-export const RegisterForm = () => {
+export const RegisterForm = ({form,onFormChange,onSubmit}) => {
 
-    const registerForm = {
-        full_name:"",
-        email: "",
-        password: "",
-        repeat_password:""
-      };
+  const {full_name,email,password,repeat_password} = form;
     
-      const {full_name,email,password,repeat_password,onFormChange} = useForm(registerForm);
-    
-
   return (
-   <form className="register-form">
+   <form className="register-form" onSubmit={(e)=>{e.preventDefault(); onSubmit();}}>
         <input className='input-field' type="text" placeholder="FullName" name="full_name" value={full_name} onChange={onFormChange}/>
         <input className='input-field' type="email" placeholder="Email" name="email" value={email} onChange={onFormChange}/>
         <input className='input-field' type="password" placeholder="Password" name="password" value={password} onChange={onFormChange}/>
         <input className='input-field' type="password" placeholder="Repeat password" name="repeat_password" value={repeat_password} onChange={onFormChange}/>
-        <Button text="Register" type="blue" height="31px" width="134px" />
+        <Button text="Register" type="blue" height="31px" width="134px" variation="input" />
    </form>
   )
 }
