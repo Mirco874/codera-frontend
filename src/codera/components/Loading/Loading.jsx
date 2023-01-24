83402a6721c0786ae55f0a9ac27584b972e606c2
Mirco Estefano Pilco Peralta <mircocuof@gmail.com>
2023-01-24 13:45:50 -0400

    added loading component

---
+++ src/codera/components/Loading/Loading.jsx
@@ -0,0 +1,7 @@
+import "./Loading.css"
+export const Loading = () => {
+  return (
+        <img className="loading" src="https://thumbs.gfycat.com/AthleticPitifulCanary-max-1mb.gif" 
+             alt="loading gif"/>
+  )
+}
