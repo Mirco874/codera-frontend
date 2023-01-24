30c6e594752fea0bb706740c510c919dcb81707d
Mirco Estefano Pilco Peralta <mircocuof@gmail.com>
2023-01-24 11:38:00 -0400

    fix: when the user wants to copy the code of the class, he enters it

--- src/codera/components/ClassCard/ClassCard.jsx
+++ src/codera/components/ClassCard/ClassCard.jsx
@@ -1,13 +1,19 @@
-import './ClassCard.css';
 import {useNavigate} from 'react-router-dom';
+import './ClassCard.css';
 
-export const ClassCard = ({id,className,classDescription,instructor}) => {
-    const navigate=useNavigate();
+export const ClassCard = ({id,className,instructor}) => {
+  const navigate=useNavigate();
+  
+  const goToClass=()=>{
+    navigate(id);
+  }
+
   return (
-    <div className='class-card' onClick={()=>{navigate(id)}}>
-        
-        <p className='sub-title1'> <b>{className}</b></p>
+    <div className='class-card'>
+      <div className='class-card-header' onClick={goToClass} >
+        <p className='subtitle1'> {className}  </p>
         <p className='body2'><b>instructor:</b> {instructor}</p>
+      </div>
         <hr />
         <p className='body2'><b>code:</b> {id} </p>
     </div>
