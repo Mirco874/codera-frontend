197c14546896b97152972fbbc254522a76da283a
Mirco Estefano Pilco Peralta <mircocuof@gmail.com>
2023-01-29 18:14:49 -0400

    fix: condition to send the token to api

--- src/hooks/useFetch/useFetch.jsx
+++ src/hooks/useFetch/useFetch.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "../../api/axios";
 
 export const useFetch = (url = "") => {
+
   const [content, setContent] = useState({
     data: null,
     isLoading: true,
@@ -11,22 +12,25 @@ export const useFetch = (url = "") => {
     const token=localStorage.getItem('token');
 
     let config = {
-      headers: {},
-    };
+      headers: {} };
 
-    if (typeof(token==="undefined")) {
+    if (typeof(token)!=="undefined") {
       config.headers = { Authorization: "Bearer " + token };
     }
 
     try {
+      
       setContent({ ...content, isLoading: true });
       const response = await axios.get(url, config);
       setContent({ data: response.data, isLoading: false });
+
     } catch (error) {
+      
       setContent({ ...content, isLoading: false });
     }
   };
-  
+
+
   useEffect(() => {
     fetchData();
   },[]);
